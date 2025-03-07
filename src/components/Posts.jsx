7df359f79db298354741a648968fb905b8889683
raj-koshta <rajkoshta.rk1@gmail.com>
2025-03-08 00:55:10 +0530

    delete implement

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,36 +1,52 @@
 import { useEffect, useState } from "react";
-import { getPost } from "../api/PostApi";
+import { deletePost, getPost } from "../api/PostApi";
 import "../App.css"
 
-export const Posts = () =>{
+export const Posts = () => {
 
     const [data, setData] = useState([]);
 
-    const getPostData = async () =>{
+    const getPostData = async () => {
         try {
-          const res = await getPost();
-          const result = res.data;
-          console.log(result);
-          setData(result);
+            const res = await getPost();
+            const result = res.data;
+            console.log(result);
+            setData(result);
         } catch (error) {
-          console.log(error);
+            console.log(error);
         }
     }
-    
-    useEffect(()=> {
-    getPostData()
+
+    useEffect(() => {
+        getPostData()
     }, [])
 
-    return  <section className="section-post">
+    // function to delete post
+
+    const handleDeletePost = async (id) => {
+        try {
+            const res = await deletePost(id);
+            if (res.status == 200) {
+                const newUpdatedPosts = data.filter((curPost)=>{
+                    return curPost.id != id;
+                })
+                setData(newUpdatedPosts);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return <section className="section-post">
         <ol>
             {
-                data.map((curElem)=>{
-                    const {id,body,title} = curElem;
+                data.map((curElem) => {
+                    const { id, body, title } = curElem;
                     return <li key={id}>
                         <p>Title: {title}</p>
                         <p>Body: {body}</p>
                         <button>Edit</button>
-                        <button className="btn-delete">Delete</button>
+                        <button className="btn-delete" onClick={() => { handleDeletePost(id) }}>Delete</button>
                     </li>
                 })
             }
